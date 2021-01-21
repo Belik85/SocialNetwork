ccb43ca388755deafd108252478847694b140c11
Belik85 <beletskyidmytro@gmail.com>
2021-01-22 00:07:07 +0100

    modify components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
-import {BrowserRouter} from "react-router-dom";
-import {addPost} from "./redux/state";
-
-// addPost('samurai')
-
-let rerenderEntireTree = () => {
-
-    ReactDOM.render(
-        <BrowserRouter>
-            <React.StrictMode>
-                <App state={state} addPost = {addPost}/>
-            </React.StrictMode>,
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-
-}
-
-rerenderEntireTree();
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+rerenderEntireTree (state);
+
+
+
+
+
+
