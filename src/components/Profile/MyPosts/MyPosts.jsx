81fb7eb577415d68cf969e2f8da36674f97cc456
Belik85 <beletskyidmytro@gmail.com>
2021-10-26 16:33:56 +0200

    modify components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,9 +5,6 @@ import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../red
 
 
 
-
-
-
 const  MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <Post message = {p.message}  likesCount = {p.likesCount}/>)
