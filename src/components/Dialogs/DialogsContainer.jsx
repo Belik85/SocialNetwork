44a9552b74768ba4493d5ae3fa2441b3f907c026
Belik85 <beletskyidmytro@gmail.com>
2021-11-15 23:26:53 +0100

    modify components

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -8,90 +8,99 @@ import StoreContext from "../../StoreContext";
 import {connect} from "react-redux";
 
 
-const DialogsContainer = (props) => {
+// const DialogsContainer = () => {
+//
+//     // let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+//
+//     // let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+//
+//     // let messagesElements = state.messages.map(m => <Message message={m.message}/>)
+//
+//     // let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
+//
+//     // let newMessageBody = state.newMessageBody;
+//
+//     // let newMessageBody = props.state.newMessageBody;
+//
+//
+//     // let d = {
+//     //     id: 1,
+//     //     name: 'Dimych'
+//     // };
+//     //
+//     // let v = {
+//     //     id: 2,
+//     //     name: 'Vitalii'
+//     // };
+//     //
+//     // let l = {
+//     //     id: 3,
+//     //     name: 'Lesha'
+//     // };
+//     //
+//     // let w = {
+//     //     id: 4,
+//     //     name: 'Witya'
+//     // };
+//     //
+//     // let k = {
+//     //     id: 5,
+//     //     name: 'Karolina'
+//     // };
+//     //
+//     // let y = {
+//     //     id: 6,
+//     //     name: 'Yana'
+//     // };
+//
+//
+//     return <StoreContext.Consumer>
+//         { store => {
+//             // let state = props.store.getState().dialogsPage;
+//             // let state = store.getState().dialogsPage;
+//             // let onSendMessageClick = () => {
+//             //     props.store.dispatch(sendMessageCreator())
+//             // };
+//             let onSendMessageClick = () => {
+//                 store.dispatch(sendMessageCreator());
+//             };
+//             // let onNewMessageChange = (body) => {
+//             //     props.store.dispatch(updateNewMessageBodyCreator(body));
+//             // };
+//             let onNewMessageChange = (body) => {
+//                 store.dispatch(updateNewMessageBodyCreator(body));
+//             }
+//             return <Dialogs updateNewMessageBody={onNewMessageChange}
+//                             sendMessage={onSendMessageClick}
+//                             // dialogsPage={state}
+//                             dialogsPage={store.getState().dialogsPage}
+//             />
+//         }
+//     }
+//     </StoreContext.Consumer>
+//
+// }
 
-    // let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-
-    // let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-
-    // let messagesElements = state.messages.map(m => <Message message={m.message}/>)
-
-    // let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
-
-    // let newMessageBody = state.newMessageBody;
-
-    // let newMessageBody = props.state.newMessageBody;
-
-
-    // let d = {
-    //     id: 1,
-    //     name: 'Dimych'
-    // };
-    //
-    // let v = {
-    //     id: 2,
-    //     name: 'Vitalii'
-    // };
-    //
-    // let l = {
-    //     id: 3,
-    //     name: 'Lesha'
-    // };
-    //
-    // let w = {
-    //     id: 4,
-    //     name: 'Witya'
-    // };
-    //
-    // let k = {
-    //     id: 5,
-    //     name: 'Karolina'
-    // };
-    //
-    // let y = {
-    //     id: 6,
-    //     name: 'Yana'
-    // };
-
-
-    return <StoreContext.Consumer>
-        {
-        (store) => {
-            // let state = props.store.getState().dialogsPage;
-            let state = store.getState().dialogsPage;
-            // let onSendMessageClick = () => {
-            //     props.store.dispatch(sendMessageCreator())
-            // };
-            let onSendMessageClick = () => {
-                store.dispatch(sendMessageCreator())
-            };
-            // let onNewMessageChange = (body) => {
-            //     props.store.dispatch(updateNewMessageBodyCreator(body));
-            // };
-            let onNewMessageChange = (body) => {
-                store.dispatch(updateNewMessageBodyCreator(body));
-            }
-            return <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick}
-                            dialogsPage={state}/>
-        }
-    }
-    </StoreContext.Consumer>
-
-}
-
-let f1 = () => {
+let mapStateToProps = (state) => {
     return {
-
+       dialogsPage: state.dialogsPage
     }
 }
 
-let f2 = () => {
+let mapDispatchToProps = (dispatch) => {
     return {
+        updateNewMessageBody: () => {
+            dispatch(sendMessageCreator());
+        },
+        sendMessage: (body) => {
+            dispatch(updateNewMessageBodyCreator(body));
+        },
+
 
     }
 }
 
-const SuperDialogsContainer = connect(f1,f2)  (Dialogs);
+const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)  (Dialogs);
 
 export default DialogsContainer;
 
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,38 +2,61 @@ import React from 'react';
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import StoreContext from "../../../StoreContext";
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
+import mapDispatchToProps from "react-redux/lib/connect/mapDispatchToProps";
+import {connect} from "react-redux";
 
 
 const MyPostsContainer = (props) => {
 
-   // let state = props.store.getState();
+    // let state = props.store.getState();
     return (
         <StoreContext.Consumer>
             {
-            (store) => {
-                // let state = props.store.getState();
-                let state = store.getState();
-                let addPost = () => {
-                    // props.store.dispatch(addPostActionCreator());
-                    store.dispatch(addPostActionCreator());
-                }
-                let onPostChange = (text) => {
-                    let action = updateNewPostTextActionCreator(text);
-                    // props.store.dispatch(action);
-                    store.dispatch(action);
-                }
-                return <MyPosts
-                    updateNewPostText={onPostChange}
-                    addPost={addPost}
-                    posts={store.getState().profilePage.posts}
-                    // posts={state.profilePage.posts}
-                    newPostText={store.getState().profilePage.newPostText}
-                    // newPostText={state.profilePage.newPostText}
-                />
-            }}
+                (store) => {
+                    // let state = props.store.getState();
+                    let state = store.getState();
+                    let addPost = () => {
+                        // props.store.dispatch(addPostActionCreator());
+                        store.dispatch(addPostActionCreator());
+                    }
+                    let onPostChange = (text) => {
+                        let action = updateNewPostTextActionCreator(text);
+                        // props.store.dispatch(action);
+                        store.dispatch(action);
+                    }
+                    return <MyPosts
+                        updateNewPostText={onPostChange}
+                        addPost={addPost}
+                        posts={store.getState().profilePage.posts}
+                        // posts={state.profilePage.posts}
+                        newPostText={store.getState().profilePage.newPostText}
+                        // newPostText={state.profilePage.newPostText}
+                    />
+                }}
         </StoreContext.Consumer>
     )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text) => {
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        }
+    }
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 
 export default MyPostsContainer;
