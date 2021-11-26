ccf1fd0deb3ad5085feac67afc97cd564baa3126
Belik85 <beletskyidmytro@gmail.com>
2021-11-26 20:25:00 +0100

    refactoring

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -59,7 +59,6 @@ const profileReducer = (state = initialState, action) => {
         // state.newPostText = '';
         // return state;
 
-
         case UPDATE_NEW_POST_TEXT: {
 
             return  {
