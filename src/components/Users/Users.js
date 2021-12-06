35114ff7cf7388a592c12b9de835a9fccac25534
Belik85 <beletskyidmytro@gmail.com>
2021-12-06 22:00:34 +0100

    refactoring

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,52 +5,54 @@ import userPhoto from "./../../../src/Pictures/Images/user.jpg"
 
 let Users = (props) => {
 
-    if(props.users.length === 0) {
+    let getUsers = () => {
+        if(props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger;
-            props.setUsers(response.data.items);
-            //     [{
-            //     id: 1,
-            //     photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-            //     followed: false,
-            //     fullName: 'Dmitry',
-            //     status: 'I am a boss',
-            //     location: {city: 'Minsk', country: 'Belarus'}
-            // },
-            //     {id: 2, followed: true, fullName: 'Ivan', status: 'No problem', location: {city: 'Moscow', country: 'Russia'}},
-            //     {
-            //         id: 3,
-            //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-            //         followed: false,
-            //         fullName: 'Roma',
-            //         status: 'You are a game',
-            //         location: {city: 'Kiev', country: 'Ukraine'}
-            //     },
-            //     {
-            //         id: 4,
-            //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR14mXbg8nV2aaoAMs3BxYS3EoOK8wR6O8yZg&usqp=CAU",
-            //         followed: true,
-            //         fullName: 'Kamil',
-            //         status: 'Send me a message',
-            //         location: {city: 'Wroclaw', country: 'Poland'}
-            //     },
-            //     {
-            //         id: 5,
-            //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-            //         followed: false,
-            //         fullName: 'Dorian',
-            //         status: 'Music of the sun',
-            //         location: {city: 'Bucharest', country: 'Romania'}
-            //     }]
-
-        })
-
-
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                debugger;
+                props.setUsers(response.data.items);
+                //     [{
+                //     id: 1,
+                //     photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+                //     followed: false,
+                //     fullName: 'Dmitry',
+                //     status: 'I am a boss',
+                //     location: {city: 'Minsk', country: 'Belarus'}
+                // },
+                //     {id: 2, followed: true, fullName: 'Ivan', status: 'No problem', location: {city: 'Moscow', country: 'Russia'}},
+                //     {
+                //         id: 3,
+                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+                //         followed: false,
+                //         fullName: 'Roma',
+                //         status: 'You are a game',
+                //         location: {city: 'Kiev', country: 'Ukraine'}
+                //     },
+                //     {
+                //         id: 4,
+                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR14mXbg8nV2aaoAMs3BxYS3EoOK8wR6O8yZg&usqp=CAU",
+                //         followed: true,
+                //         fullName: 'Kamil',
+                //         status: 'Send me a message',
+                //         location: {city: 'Wroclaw', country: 'Poland'}
+                //     },
+                //     {
+                //         id: 5,
+                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+                //         followed: false,
+                //         fullName: 'Dorian',
+                //         status: 'Music of the sun',
+                //         location: {city: 'Bucharest', country: 'Romania'}
+                //     }]
+            })
+        }
     }
 
 
+
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
