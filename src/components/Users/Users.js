e8ce2eb4946076e14a346a32aedcda5960aa685f
Belik85 <beletskyidmytro@gmail.com>
2021-12-15 21:45:43 +0100

    refactoring

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,7 +5,6 @@ import axios from "axios";
 
 
 class Users extends React.Component {
-    
 
 
     // constructor(props) {
@@ -24,46 +23,59 @@ class Users extends React.Component {
     //
     //         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
     //             this.props.setUsers(response.data.items);
-                //     [{
-                //     id: 1,
-                //     photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-                //     followed: false,
-                //     fullName: 'Dmitry',
-                //     status: 'I am a boss',
-                //     location: {city: 'Minsk', country: 'Belarus'}
-                // },
-                //     {id: 2, followed: true, fullName: 'Ivan', status: 'No problem', location: {city: 'Moscow', country: 'Russia'}},
-                //     {
-                //         id: 3,
-                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-                //         followed: false,
-                //         fullName: 'Roma',
-                //         status: 'You are a game',
-                //         location: {city: 'Kiev', country: 'Ukraine'}
-                //     },
-                //     {
-                //         id: 4,
-                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR14mXbg8nV2aaoAMs3BxYS3EoOK8wR6O8yZg&usqp=CAU",
-                //         followed: true,
-                //         fullName: 'Kamil',
-                //         status: 'Send me a message',
-                //         location: {city: 'Wroclaw', country: 'Poland'}
-                //     },
-                //     {
-                //         id: 5,
-                //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
-                //         followed: false,
-                //         fullName: 'Dorian',
-                //         status: 'Music of the sun',
-                //         location: {city: 'Bucharest', country: 'Romania'}
-                //     }]
+    //     [{
+    //     id: 1,
+    //     photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+    //     followed: false,
+    //     fullName: 'Dmitry',
+    //     status: 'I am a boss',
+    //     location: {city: 'Minsk', country: 'Belarus'}
+    // },
+    //     {id: 2, followed: true, fullName: 'Ivan', status: 'No problem', location: {city: 'Moscow', country: 'Russia'}},
+    //     {
+    //         id: 3,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+    //         followed: false,
+    //         fullName: 'Roma',
+    //         status: 'You are a game',
+    //         location: {city: 'Kiev', country: 'Ukraine'}
+    //     },
+    //     {
+    //         id: 4,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR14mXbg8nV2aaoAMs3BxYS3EoOK8wR6O8yZg&usqp=CAU",
+    //         followed: true,
+    //         fullName: 'Kamil',
+    //         status: 'Send me a message',
+    //         location: {city: 'Wroclaw', country: 'Poland'}
+    //     },
+    //     {
+    //         id: 5,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT32B6kRnn-YYaHQ1PBibKZlH_3UFd-5Th-4w&usqp=CAU",
+    //         followed: false,
+    //         fullName: 'Dorian',
+    //         status: 'Music of the sun',
+    //         location: {city: 'Bucharest', country: 'Romania'}
+    //     }]
     //         })
     //     }
     // }
 
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
+            {/*<button onClick={this.getUsers}>Get Users</button>*/}
+
+            <div>
+                <span>1</span>
+                <span className={styles.selectedPage}>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span>8</span>
+                <span>9</span>
+                <span>10</span>
+            </div>
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>
