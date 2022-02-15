b350537804d4a2e75d9f656f72d387ef74a9d260
Belik85 <beletskyidmytro@gmail.com>
2022-02-15 09:10:50 +0100

    reduce code in a ComponentContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,6 +9,7 @@ import {withRouter} from "react-router-dom";
 class ProfileContainer extends React.Component {
 
 
+
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
