f5762b8c6c2a54fb4507f0ebb8a563cc24fef7d3
Belik85 <beletskyidmytro@gmail.com>
2022-04-06 20:50:36 +0200

    refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -28,11 +28,24 @@ export const usersAPI = {
         return instance.delete(`follow/${userId}`)
         // return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`)
     },
+    // getProfile(userId) {
+    //     return instance.get(`profile/` + userId);
+    //     // return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId);
+    // },
     getProfile(userId) {
-        return instance.get(`profile/` + userId);
-        // return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId);
+        return profileAPI.getProfile(userId)
+
     },
 }
+
+export const profileAPI = {
+    getProfile(userId) {
+        return instance.get(`profile/` + userId);
+
+    },
+}
+
+
 
 // export const getUsers = (currentPage = 1, pageSize = 10) => {
 // return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
